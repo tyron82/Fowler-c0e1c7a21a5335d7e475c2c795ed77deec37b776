ace91c116f693c0950a1eab2babd9b64c9b747a8
Danilo Mischke <DA_MI@ec4u.de>
2022-05-12 10:15:03 +0200

    Removed all comments (because I hate comments).

--- src/main/Customer.java
+++ src/main/Customer.java
@@ -29,19 +29,14 @@ public class Customer {
         while (enum_rentals.hasMoreElements()) {
             double thisAmount = 0;
             Rental each = (Rental) enum_rentals.nextElement();
-            // determine amounts for each line
             thisAmount = amountFor(each);
-            // add frequent renter points
             frequentRenterPoints++;
-            // add bonus for a two day new release rental
             if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1)
                 frequentRenterPoints++;
-            // show figures for this rental
             result += "\t" + each.getMovie().getTitle() + "\t" + "\t" + each.getDaysRented() + "\t"
                     + String.valueOf(thisAmount) + "\n";
             totalAmount += thisAmount;
         }
-        // add footer lines
         result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
         result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
         return result;
--- src/main/Program.java
+++ src/main/Program.java
@@ -1,15 +1,6 @@
 package main;
 
-/**
- * Note that Java console applications need to be run through the java runtime
- * by running "java -jar JarFile.jar" in the command line.
- * Java console applications can not be previewed in the Compilr IDE, only
- * applets can.
- */
 public class Program {
-    /**
-     * This is the main entry point for the application
-     */
 
     public static void main(String args[]) {
         String result;
@@ -25,4 +16,5 @@ public class Program {
         result = c1.statement();
         System.out.println(result);
     }
+
 }
