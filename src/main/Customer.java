6e2ddcbed62ec74d5c5cad7e79065b0048578fc2
Danilo Mischke <DA_MI@ec4u.de>
2022-05-12 10:46:18 +0200

    Refactored extracted functions

--- src/main/Customer.java
+++ src/main/Customer.java
@@ -29,69 +29,69 @@ public class Customer {
             double thisAmount = 0;
             Rental each = (Rental) enum_rentals.nextElement();
             thisAmount = amountFor(each);
-            frequentRenterPoints = updateFrequentRenterPoints(frequentRenterPoints, each);
-            result = createResultBody(result, thisAmount, each);
+            frequentRenterPoints += additionalFrequentRenterPoints(each);
+            result += createResultBody(thisAmount, each);
             totalAmount += thisAmount;
         }
-        result = createResultFooter(totalAmount, frequentRenterPoints, result);
+        result += createResultFooter(totalAmount, frequentRenterPoints);
         return result;
     }
 
-    private int updateFrequentRenterPoints(int frequentRenterPoints, Rental each) {
-        frequentRenterPoints++;
+    private int additionalFrequentRenterPoints(Rental each) {
+        int additionalFrequentRenterPoints = 1;
         if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1)
-            frequentRenterPoints++;
-        return frequentRenterPoints;
+            additionalFrequentRenterPoints++;
+        return additionalFrequentRenterPoints;
     }
 
     private String createResultHeader() {
-        String result = "Rental Record for " + this.getName() + "\n";
-        result += "\t" + "Title" + "\t" + "\t" + "Days" + "\t" + "Amount" + "\n";
-        return result;
+        String resultHeader = "Rental Record for " + this.getName() + "\n";
+        resultHeader += "\t" + "Title" + "\t" + "\t" + "Days" + "\t" + "Amount" + "\n";
+        return resultHeader;
     }
 
-    private String createResultBody(String result, double thisAmount, Rental each) {
-        result += "\t" + each.getMovie().getTitle() + "\t" + "\t" + each.getDaysRented() + "\t"
+    private String createResultBody(double thisAmount, Rental each) {
+        String resultBody = "\t" + each.getMovie().getTitle() + "\t" + "\t" + each.getDaysRented() + "\t"
                 + String.valueOf(thisAmount) + "\n";
-        return result;
+        return resultBody;
     }
 
-    private String createResultFooter(double totalAmount, int frequentRenterPoints, String result) {
-        result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
-        result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
-        return result;
+    private String createResultFooter(double totalAmount, int frequentRenterPoints) {
+        String resultFooter = "Amount owed is " + String.valueOf(totalAmount) + "\n";
+        resultFooter += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
+        return resultFooter;
     }
 
     private double amountFor(Rental each) {
         double thisAmount = 0;
         switch (each.getMovie().getPriceCode()) {
             case Movie.REGULAR:
-                thisAmount = calculateRegularAmount(each, thisAmount);
+                thisAmount = calculateRegularAmount(each);
                 break;
             case Movie.NEW_RELEASE:
-                thisAmount = calculateNewReleaseAmount(each, thisAmount);
+                thisAmount = calculateNewReleaseAmount(each);
                 break;
             case Movie.CHILDRENS:
-                thisAmount = calculateChildrenAmount(each, thisAmount);
+                thisAmount = calculateChildrenAmount(each);
                 break;
         }
         return thisAmount;
     }
 
-    private double calculateRegularAmount(Rental each, double thisAmount) {
-        thisAmount += 2;
+    private double calculateRegularAmount(Rental each) {
+        double thisAmount = 2;
         if (each.getDaysRented() > 2)
             thisAmount += (each.getDaysRented() - 2) * 1.5;
         return thisAmount;
     }
 
-    private double calculateNewReleaseAmount(Rental each, double thisAmount) {
-        thisAmount += each.getDaysRented() * 3;
+    private double calculateNewReleaseAmount(Rental each) {
+        double thisAmount = each.getDaysRented() * 3;
         return thisAmount;
     }
 
-    private double calculateChildrenAmount(Rental each, double thisAmount) {
-        thisAmount += 1.5;
+    private double calculateChildrenAmount(Rental each) {
+        double thisAmount = 1.5;
         if (each.getDaysRented() > 3)
             thisAmount += (each.getDaysRented() - 3) * 1.5;
         return thisAmount;
