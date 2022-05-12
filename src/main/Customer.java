4bf1f1d8473ac39d4636aa02eb37326e89fb6195
Danilo Mischke <DA_MI@ec4u.de>
2022-05-12 10:35:47 +0200

    Extracted functions in customer

--- src/main/Customer.java
+++ src/main/Customer.java
@@ -23,20 +23,40 @@ public class Customer {
         double totalAmount = 0;
         int frequentRenterPoints = 0;
         Enumeration enum_rentals = rentals.elements();
-        String result = "Rental Record for " + this.getName() + "\n";
-        result += "\t" + "Title" + "\t" + "\t" + "Days" + "\t" + "Amount" + "\n";
+        String result = createResultHeader();
 
         while (enum_rentals.hasMoreElements()) {
             double thisAmount = 0;
             Rental each = (Rental) enum_rentals.nextElement();
             thisAmount = amountFor(each);
-            frequentRenterPoints++;
-            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1)
-                frequentRenterPoints++;
-            result += "\t" + each.getMovie().getTitle() + "\t" + "\t" + each.getDaysRented() + "\t"
-                    + String.valueOf(thisAmount) + "\n";
+            frequentRenterPoints = updateFrequentRenterPoints(frequentRenterPoints, each);
+            result = createResultBody(result, thisAmount, each);
             totalAmount += thisAmount;
         }
+        result = createResultFooter(totalAmount, frequentRenterPoints, result);
+        return result;
+    }
+
+    private int updateFrequentRenterPoints(int frequentRenterPoints, Rental each) {
+        frequentRenterPoints++;
+        if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1)
+            frequentRenterPoints++;
+        return frequentRenterPoints;
+    }
+
+    private String createResultHeader() {
+        String result = "Rental Record for " + this.getName() + "\n";
+        result += "\t" + "Title" + "\t" + "\t" + "Days" + "\t" + "Amount" + "\n";
+        return result;
+    }
+
+    private String createResultBody(String result, double thisAmount, Rental each) {
+        result += "\t" + each.getMovie().getTitle() + "\t" + "\t" + each.getDaysRented() + "\t"
+                + String.valueOf(thisAmount) + "\n";
+        return result;
+    }
+
+    private String createResultFooter(double totalAmount, int frequentRenterPoints, String result) {
         result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
         result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
         return result;
@@ -46,19 +66,34 @@ public class Customer {
         double thisAmount = 0;
         switch (each.getMovie().getPriceCode()) {
             case Movie.REGULAR:
-                thisAmount += 2;
-                if (each.getDaysRented() > 2)
-                    thisAmount += (each.getDaysRented() - 2) * 1.5;
+                thisAmount = calculateRegularAmount(each, thisAmount);
                 break;
             case Movie.NEW_RELEASE:
-                thisAmount += each.getDaysRented() * 3;
+                thisAmount = calculateNewReleaseAmount(each, thisAmount);
                 break;
             case Movie.CHILDRENS:
-                thisAmount += 1.5;
-                if (each.getDaysRented() > 3)
-                    thisAmount += (each.getDaysRented() - 3) * 1.5;
+                thisAmount = calculateChildrenAmount(each, thisAmount);
                 break;
         }
+        return thisAmount;
+    }
+
+    private double calculateRegularAmount(Rental each, double thisAmount) {
+        thisAmount += 2;
+        if (each.getDaysRented() > 2)
+            thisAmount += (each.getDaysRented() - 2) * 1.5;
+        return thisAmount;
+    }
+
+    private double calculateNewReleaseAmount(Rental each, double thisAmount) {
+        thisAmount += each.getDaysRented() * 3;
+        return thisAmount;
+    }
+
+    private double calculateChildrenAmount(Rental each, double thisAmount) {
+        thisAmount += 1.5;
+        if (each.getDaysRented() > 3)
+            thisAmount += (each.getDaysRented() - 3) * 1.5;
         return thisAmount;
     }
 
